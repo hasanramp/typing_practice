92ba8d34b2bcfa8846b89720e89b11b972692585
hasan <rampurawalahasan4@gmail.com>
2021-10-12 15:10:09 +0530

    Added space before printing the results. Just looks a bit better

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ try:
         index += 1
 except IndexError:
     print('the number of words entered are more than the number of words printed out. You probably made some mistake while typing which resulted in this. Please try again')
-    
+
 time_taken = round(time.time() - init_time, 1)
 raw_wpm, total_number_of_char = tt.calculate_wpm(words, time_taken)
 wpm, correct_number_of_char = tt.calculate_wpm(correct_words, time_taken)
@@ -36,11 +36,12 @@ accuracy = len(correct_words) / len(words) * 100
 
 print('========================================================================================')
 print('\n \n')
-print(f'Accuracy = {accuracy}%\n')
-print(f'time taken = {time_taken}\n')
-print(f'Raw wpm: {raw_wpm}\n')
-print(f'wpm = {wpm}\n')
+space = '                         '
+print(f'{space}Accuracy = {accuracy}%\n')
+print(f'{space}time taken = {time_taken}\n')
+print(f'{space}Raw wpm: {raw_wpm}\n')
+print(f'{space}wpm = {wpm}\n')
 incorrect_words_str = ', '.join(incorrect_words)
-print(f'incorrect words: {incorrect_words_str}\n')
-print(f'correct number of characters: {correct_number_of_char}\n')
-print(f'total number of characters: {total_number_of_char}\n')
+print(f'{space}incorrect words: {incorrect_words_str}\n')
+print(f'{space}correct number of characters: {correct_number_of_char}\n')
+print(f'{space}total number of characters: {total_number_of_char}\n')
