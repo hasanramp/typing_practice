5077fe9d334ef82e36d26ef47587d5c5f0777078
hasan <rampurawalahasan4@gmail.com>
2021-10-14 21:58:51 +0530

    Made changes to configuration.py with which now user can view the options for each configuration so as to not mess up the configuration. Cool feature. Like it!

--- configure.py
+++ configure.py
@@ -7,8 +7,24 @@ from __init__ import get_configuration
 
 what_to_configure = input('what to configure? (type help to get more information): ')
 
+def get_options(setting):
+    configuration_file_name = 'configuration.json'
+    configuration_file = open(configuration_file_name, 'r')
+    configuration_json = json.load(configuration_file)
+    return configuration_json[setting]['options']
+
+
 if what_to_configure == 'start method':
     method = input('new start method: ')
+    if method == '!options':
+        options = get_options('start method')
+        options_str = ''
+        for option in options:
+            option_with_comma = option + ','
+            options_str += option_with_comma
+        
+        print('options are: ' + options_str)
+        exit()
     n_of_words, start_method, difficulty = get_configuration()
     configuration_file_name = 'configuration.json'
     configuration_file = open(configuration_file_name, 'w')
@@ -29,7 +45,17 @@ if what_to_configure == 'start method':
     json.dump(new_config, configuration_file, indent=4)
 
 elif what_to_configure == 'no of words':
-    new_no_of_words = int(input('new number of words: '))
+    new_no_of_words = input('new number of words: ')
+    if new_no_of_words == '!options':
+        options = get_options('no. of words')
+        options_str = ''
+        for option in options:
+            option_with_comma = option + ','
+            options_str += option_with_comma
+        print('options are: ' + options_str)
+        exit()
+    else:
+        new_no_of_words = int(new_no_of_words)
     n_of_words, start_method, difficulty = get_configuration()
     configuration_file_name = 'configuration.json'
     configuration_file = open(configuration_file_name, 'w')
@@ -50,7 +76,15 @@ elif what_to_configure == 'no of words':
     json.dump(new_config, configuration_file, indent=4)
 
 elif what_to_configure == 'difficulty':
-    new_diffculty = int(input('new difficulty: '))
+    new_diffculty = input('new difficulty: ')
+    if new_diffculty == '!options':
+        options = get_options('difficulty')
+        options_str = ''
+        for option in options:
+            option_with_comma = option + ','
+            options_str += option_with_comma
+        print('options are: ' + options_str)
+        exit()
     n_of_words, start_method, difficulty = get_configuration()
     configuration_file_name = 'configuration.json'
     configuration_file = open(configuration_file_name, 'w')
